c2da1ab60011c94f50b07fca2c536419122dd2b5
Qndtl <johd321@naver.com>
2021-05-15 17:51:54 +0900

    AppLoading - preload font and image

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
+import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import * as Font from 'expo-font';
+import { Asset } from 'expo-asset';
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const onFinish = () => setLoading(false)
+  const preload = () => {
+    const fontsToLoad = [Ionicons.font];
+    const fontPromises = fontsToLoad.map(font => Font.loadAsync(font));
+    const imagesToLoad = ["https://1000logos.net/wp-content/uploads/2017/02/Instagram-Logo.png"]
+    const imagePromises = imagesToLoad.map(image => Asset.loadAsync(image))
+    return Promise.all([...fontPromises, ...imagePromises]);
+  }
+  if (loading) {
+    return <AppLoading startAsync={preload} onFinish={onFinish} onError={console.warn} />
+  }
   return (
     <View style={styles.container}>
-      <Text>This is React Native with Expo</Text>
+      <Text>Ho~~</Text>
       <StatusBar style="auto" />
     </View>
   );
